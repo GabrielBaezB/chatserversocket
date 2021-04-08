fdd22284c62a925d0efb581c9424a25cfa0dc35a
GabrielBaezB <gabooo@live.com>
2021-04-07 20:11:34 -0400

    Funciones base del servidor

--- ChatServerApp/ChatServerApp/Program.cs
+++ ChatServerApp/ChatServerApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ChatServerApp.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,36 @@ namespace ChatServerApp
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando servidor {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if(servidor.Iniciar())
+            {
+                while(true)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Esperando conexion de Cliente");
+                    if(servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.WriteLine("Cliente Conectado!");
+                        Console.WriteLine("S: Hola Cliente!");
+                        servidor.Escribir("Hola Cliente!");
+                        string mensaje = servidor.Leer();
+                        Console.WriteLine("c: {0}", mensaje);
+                        //<CR><LF> salto de linea
+                        servidor.CerrarConexion();
+
+                    }
+                }          
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error al iniciar al servidor");
+                Console.ReadKey();
+            }
         }
     }
 }
