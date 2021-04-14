c4764383ea4241b0e0d1dff06877335de71eb20c
GabrielBaezB <gabooo@live.com>
2021-04-13 21:42:58 -0400

    Generar funcionalidad del server

--- ChatServerApp/ClienteSocket/Program.cs
+++ ChatServerApp/ClienteSocket/Program.cs
@@ -1,12 +1,13 @@
-﻿using ChatServerApp.Comunicacion;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChatServerApp
+namespace ClienteSocket
 {
     class Program
     {
@@ -16,33 +17,23 @@ namespace ChatServerApp
             Console.WriteLine("Iniciando servidor {0}", puerto);
             ServerSocket servidor = new ServerSocket(puerto);
 
-            if(servidor.Iniciar())
+            if (servidor.Iniciar())
             {
-                while(true)
+                while (true)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("Esperando conexion de Cliente");
-                    if(servidor.ObtenerCliente())
+                    if (servidor.ObtenerCliente())
                     {
                         Console.ForegroundColor = ConsoleColor.DarkMagenta;
                         Console.WriteLine("Cliente Conectado!");
-                        //Console.WriteLine("S: Hola Cliente!");
-                        //servidor.Escribir("Hola Cliente!");
-                        //string mensaje = servidor.Leer();
-                        //Console.WriteLine("c: {0}", mensaje);
-                        ////<CR><LF> salto de linea
-                        //servidor.CerrarConexion();
-
-                        //implementar protocolo
-                        //1. El cliente inicia la comunicacion
-                        //2. El servidor responde
-                        //3. Si alguno de los extremos dice chao, la comunicacion se finaliza
+                      
                         string mensaje = "";
-                            while(mensaje.ToLower() != "chao")
+                        while (mensaje.ToLower() != "chao")
                         {
                             mensaje = servidor.Leer();
                             Console.WriteLine("C:{0}", mensaje);
-                            if(mensaje.ToLower() != "chao")
+                            if (mensaje.ToLower() != "chao")
                             {
                                 Console.WriteLine("Digame lo que quiere decirle guruguru");
                                 mensaje = Console.ReadLine().Trim();
@@ -53,7 +44,7 @@ namespace ChatServerApp
                         servidor.CerrarConexion();
 
                     }
-                }          
+                }
             }
             else
             {
